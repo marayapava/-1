75b2f9b887573f8da862f85e6354b69554af7cbd
marayapava <48051704+marayapava@users.noreply.github.com>
2019-10-17 22:58:14 +0300

    Add files via upload

--- task1.c
+++ task1.c
@@ -12,41 +12,50 @@ int main(int argc, char** argv){
 	int i = read (fd1, buf, 1);
 	int a;
 	int b = 0;
-	int k = 0;
+	int k = 1;
+	int m = 0;
 	while (i != 0) {
 		while ((buf[0] != ' ') && (i != 0)){
-			if ((buf[0] <= '9') && (buf[0] >= '0')){
-				a = atoi(buf);
-				b = b + a;
-				k = k + 1;
+			if (((buf[0] <= '9') && (buf[0] >= '0')) || (buf[0] == '-')){
+				if (buf[0] == '-') {
+					k = 2;
+					}
+				else{
+					a = atoi(buf);
+                                        b = b + a;
+                                        k = k + 1;
+					}
 				}
 			else{
-				lseek(fd1, -(k+1), SEEK_CUR);
+				lseek(fd1, -(k), SEEK_CUR);
+				m = m + 1;
 				while ((buf[0] != ' ') && (i != 0)){
 					i = read (fd1, buf, 1);
 					write (fd2, buf, 1);
 				}
-				k = 0;
+				k = 1;
 				b = 0;
-				}
-			i = read(fd1, buf, 1);
+				m = 0;
 			}
-		if (((b % 3 == 0) && (a != 0) && (a != 5)) && (b != 0))
+			i = read(fd1, buf, 1);
+		}
+		if (((b % 3 == 0) && (a != 0) && (a != 5)) && (b != 0) && (m == 0))
 			write(fd2, "bizz ", 5); 
-		if (((a == 0) || (a == 5) && (b % 3 != 0)) && (b != 0))
+		if (((a == 0) || (a == 5) && (b % 3 != 0)) && (b != 0) && (m == 0))
 			write(fd2, "buzz ", 5);
-		if (((a == 0) || (a == 5) && (b % 3 == 0)) && (b != 0))
+		if (((a == 0) || (a == 5) && (b % 3 == 0)) && (b != 0) && (m == 0))
 			write(fd2, "bizzbuzz ", 9);
-		if (((a != 0) && (a != 5) && (b % 3 != 0)) && (b != 0)){
-			lseek(fd1, -(k+1), SEEK_CUR);
-			for (int j = 0; j <= k; j++){
+		if (((a != 0) && (a != 5) && (b % 3 != 0)) && (b != 0) && (m == 0)){
+			lseek(fd1, -(k), SEEK_CUR);
+			for (int j = 0; j < k; j++){
 				read(fd1, buf, 1);
 				write(fd2, buf, 1);
 				}
 			}
 		i = read(fd1, buf, 1);
-		k = 0;
+		k = 1;
 		b = 0;
+		m = 0;
 		}
 	close(fd1);
 	close(fd2);
